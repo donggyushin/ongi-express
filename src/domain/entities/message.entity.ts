3f2d1980dc9b08b250a7215f208a6bb7175b0c9c
신동규 <donggyu9410@gmail.com>
2025-08-30 20:39:01 +0900

    add message type

--- src/domain/entities/message.entity.ts
+++ src/domain/entities/message.entity.ts
@@ -3,6 +3,7 @@ export class Message {
     public readonly id: string,
     public readonly writerProfileId: string,
     public readonly text: string,
+    public readonly messageType?: string,
     public readonly createdAt: Date = new Date(),
     public readonly updatedAt: Date = new Date()
   ) {}
@@ -12,6 +13,7 @@ export class Message {
       id: this.id,
       writerProfileId: this.writerProfileId,
       text: this.text,
+      messageType: this.messageType,
       createdAt: this.createdAt.toISOString(),
       updatedAt: this.updatedAt.toISOString()
     };
